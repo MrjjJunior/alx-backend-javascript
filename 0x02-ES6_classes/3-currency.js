4ba3a45b66238e1321715a4016397e5124154df2
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-03 15:48:35 +0200

    classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+export default class Currency {
+    constructor(code, name) {
+      if (typeof code !== 'string' || typeof name !== 'string') {
+        throw new TypeError('Code and name must be strings');
+      }
+      this._code = code;
+      this._name = name;
+    }
+  
+    get code() {
+      return this._code;
+    }
+  
+    set code(newCode) {
+      if (typeof newCode !== 'string') throw new TypeError('Code must be a string');
+      this._code = newCode;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(newName) {
+      if (typeof newName !== 'string') throw new TypeError('Name must be a string');
+      this._name = newName;
+    }
+  
+    displayFullCurrency() {
+      return `${this._name} (${this._code})`;
+    }
+  }
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+    constructor(sqft) {
+      if (new.target === Building) {
+        throw new Error('Building is an abstract class and cannot be instantiated directly');
+      }
+      if (typeof sqft !== 'number') throw new TypeError('Square feet must be a number');
+      this._sqft = sqft;
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+  
+    evacuationWarningMessage() {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
