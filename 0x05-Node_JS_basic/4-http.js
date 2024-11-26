d05e9311ef9d3f11520671d129ead62c52da172f
Tshepiso <tlhongtshepiso2@gmail.com>
2024-11-26 22:19:08 +0200

    HTTP

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,3 +1,22 @@
 /** 
  * Creating a small HTTP server 
 */
+
+const http = require('http');
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const response = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', response.length);
+  res.write(Buffer.from(response));
+});
+
+app.listen(PORT, HOST,() => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
